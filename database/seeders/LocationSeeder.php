fb4079bfbc463ce3fcc6ab34566ea1421b6f9136
Kate <cathrinakatebolanos@gmail.com>
2025-12-17 05:15:35 +0800

    Sensor Model, Update Location Seeder

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -15,6 +15,7 @@ class LocationSeeder extends Seeder
     {
         $value =
             [
+<<<<<<< Updated upstream
                 ["id" => "1", "location_code" => "SEP", "location_name" => "SEP", "pid" => null, "created_at" => "2025-03-02 09:26:56", "updated_at" => "2025-03-02 09:26:56", "deleted_at" => null],
                 ["id" => "2", "location_code" => "EMS", "location_name" => "EMS", "pid" => "1", "created_at" => "2025-03-02 09:28:08", "updated_at" => "2025-03-02 09:28:08", "deleted_at" => null],
                 ["id" => "3", "location_code" => "Injection", "location_name" => "Injection", "pid" => "1", "created_at" => "2025-03-02 09:28:59", "updated_at" => "2025-03-02 09:28:59", "deleted_at" => null],
@@ -39,6 +40,17 @@ class LocationSeeder extends Seeder
                 ["id" => "22", "location_code" => "A1 reflow", "location_name" => "A1 reflow", "pid" => "21", "created_at" => null, "updated_at" => null, "deleted_at" => null],
                 ["id" => "23", "location_code" => "B5 reflow", "location_name" => "B5 reflow", "pid" => "21", "created_at" => null, "updated_at" => null, "deleted_at" => null],
                 ["id" => "24", "location_code" => "EOL", "location_name" => "EOL", "pid" => "13", "created_at" => null, "updated_at" => null, "deleted_at" => null]
+=======
+                ["id" => "1", "location_code" => "Uratex", "location_name" => "Uratex", "pid" => null, "created_at" => "2025-03-02 09:26:56", "updated_at" => "2025-03-02 09:26:56", "deleted_at" => null],
+                ["id" => "2", "location_code" => "Valenzuela", "location_name" => "Valenzuela", "pid" => "1", "created_at" => "2025-03-02 09:28:08", "updated_at" => "2025-03-02 09:28:08", "deleted_at" => null],
+                ["id" => "3", "location_code" => "Admin Building", "location_name" => "Admin Building", "pid" => "2", "created_at" => "2025-03-02 09:28:59", "updated_at" => "2025-03-02 09:28:59", "deleted_at" => null],
+                ["id" => "4", "location_code" => "Building No. 18", "location_name" => "Building No. 18", "pid" => "2", "created_at" => "2025-03-02 09:29:11", "updated_at" => "2025-03-02 09:29:11", "deleted_at" => null],
+                ["id" => "5", "location_code" => "Building No. 12", "location_name" => "Building No. 12", "pid" => "2", "created_at" => "2025-03-02 09:30:20", "updated_at" => "2025-03-02 09:30:20", "deleted_at" => null],
+                ["id" => "6", "location_code" => "Building No. 13", "location_name" => "Building No. 13", "pid" => "2", "created_at" => "2025-03-02 09:30:32", "updated_at" => "2025-03-02 09:30:32", "deleted_at" => null],
+                ["id" => "7", "location_code" => "Building No. 11", "location_name" => "Building No. 11", "pid" => "2", "created_at" => "2025-03-02 09:31:51", "updated_at" => "2025-03-02 09:31:51", "deleted_at" => null],
+                ["id" => "8", "location_code" => "Building No. 9", "location_name" => "Building No. 9", "pid" => "2", "created_at" => "2025-03-02 09:32:04", "updated_at" => "2025-03-02 09:32:04", "deleted_at" => null],
+                ["id" => "9", "location_code" => "Building No. 17", "location_name" => "Building No. 17", "pid" => "2", "created_at" => "2025-03-02 09:32:30", "updated_at" => "2025-03-02 09:32:30", "deleted_at" => null],
+>>>>>>> Stashed changes
 
             ];
 
--- database/seeders/SensorModelSeeder.php
+++ database/seeders/SensorModelSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\SensorModel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,26 @@ class SensorModelSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $value = [
+            [
+                "id" => 1,
+                "sensor_model" => "CVM-C10",
+                "sensor_brand" => "Circutor",
+                "sensor_type_id" => 2,
+                "sensor_reg_address" => "62,64,66,2,18,34,48,54,220"
+            ],
+        ];
+
+        foreach ($value as $item) {
+            SensorModel::updateOrCreate(
+                ['id' => $item['id']],
+                [
+                    'sensor_model' => $item['sensor_model'],
+                    'sensor_brand' => $item['sensor_brand'],
+                    'sensor_type_id' => $item['sensor_type_id'],
+                    'sensor_reg_address' => $item['sensor_reg_address'],
+                ]
+            );
+        }
     }
 }
